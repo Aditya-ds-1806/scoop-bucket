122472e06cbf41675ba74e933ea6aef6839fe7a8
Aditya DS <adithya18062000@gmail.com>
2026-01-19 22:53:56 +0530

    update: upstream parameter in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Aditya-ds-1806/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
